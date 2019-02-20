145d3f56ce1971c1fd1f163872a05f4d0052645e
amirhossein baghaie <Ba.A168@sadjad.ac.ir>
2019-02-20 23:03:41 +0330

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ height = photo.size[1]
 
 for y in range(0, height):
     print (y)
-    new_div_tag = bs4.BeautifulSoup('<div class="shit" style="width:auto; height:5px;" id="'+str(y)+'"></div>',"html.parser")
+    new_div_tag = bs4.BeautifulSoup('<div class="YYY" style="width:auto; height:5px;" id="'+str(y)+'"></div>',"html.parser")
     soup.body.insert(len(soup.body.contents),new_div_tag)
     for x in range(0, width):
         RGB = photo.getpixel((x,y))
         R,G,B = RGB 
-        new_div_tag = bs4.BeautifulSoup('<span class="fuck" style="background-color: rgb('+str(R)+','+str(G)+','+str(B)+')"></span>',"html.parser")
+        new_div_tag = bs4.BeautifulSoup('<span class="XXX" style="background-color: rgb('+str(R)+','+str(G)+','+str(B)+')"></span>',"html.parser")
         soup.find(id=y).append(new_div_tag)
         # soup.body.insert(3,new_div_tag)
 
